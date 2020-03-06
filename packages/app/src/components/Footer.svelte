979c5e7379506ef2a7afdc60543161703876f782
josefaidt <josef.aidt@gmail.com>
2020-03-06 07:40:32 -0600

    add footer, body min height

--- packages/app/src/components/Footer.svelte
+++ packages/app/src/components/Footer.svelte
@@ -1,18 +1,25 @@
 <script>
   export let author = '@josefaidt'
+  export let authorUrl = 'https://twitter.com/josefaidt'
 </script>
 
 <footer>
-  <span>Made with <span id="heart">♥</span> by {author}</span>
+  <span>Made with <span id="heart">♥</span>
+    <a href="https://twitter.com/josefaidt" target="_blank" rel="noreferrer noopener">@josefaidt</a>
+  </span>
 </footer>
 
 <style>
   footer {
-    position: fixed;
+    position: absolute;
     bottom: 0;
     left: 0;
     right: 0;
     text-align: center;
+  }
+
+  footer a {
+    color: var(--theme-primary);
   }
 
   #heart {
