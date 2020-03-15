9610c1ad18323c45c4900d7161c13ee18c25cb59
josefaidt <josef.aidt@gmail.com>
2020-03-15 16:16:18 -0500

    feat: add custom buttons with styles

---
+++ packages/app/src/components/Button.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<button on:click>
+  <slot>
+  </slot>
+</button>
+
+<style>
+  button {
+    box-shadow: 
+    0.3em 0.3em 0 0 var(--theme-primary),
+    inset 0.3em 0.3em 0 0 var(--theme-primary);
+    transition: 0.25s;
+    cursor: pointer;
+    background-color: transparent;
+    padding: 0.5rem 1rem;
+    color: var(--theme-text);
+    text-transform: uppercase;
+  }
+  button:hover,
+  button:focus {
+    box-shadow: 
+      0 0 0 0 var(--theme-primary),
+      inset 6em 3.5em 0 0 var(--theme-primary);
+    border-color: var(--theme-primary);
+    color: var(--theme-background);
+  }
+</style>
--- packages/app/src/components/Header.svelte
+++ packages/app/src/components/Header.svelte
@@ -1,10 +1,12 @@
 <script>
   import { getContext } from 'svelte'
   import { ThemeToggle } from 'svelte-themer'
+  import Button from './Button.svelte'
   import logo from '../img/logo_theme.svg'
   let title = 'Twin Pines'
 
   const { login, logout, isAuthenticated } = getContext('auth')
+  const { theme, toggle } = getContext('theme')
   // let login = async () => await $Auth.loginWithRedirect({
   //   redirect_uri: window.location.origin 
   // })
@@ -13,16 +15,20 @@
 
 <header>
   <!-- <h1>{title}</h1> -->
-  {@html logo}
-  <nav>
-    <ThemeToggle />
-    {#if $isAuthenticated}
-      <button on:click={() => logout()}>Logout</button>
-    {:else}
-      <button on:click={() => login()}>Login</button>
-    {/if}
-    <!-- <button on:click={login()}>Login</button> -->
-  </nav>
+  <div class="logo__container">
+    {@html logo}
+  </div>
+  <div class="nav__container">
+    <nav>
+      <Button on:click={toggle}>{$theme.name}</Button>
+      {#if $isAuthenticated}
+        <Button on:click={() => logout()}>Logout</Button>
+      {:else}
+        <Button on:click={() => login()}>Login</Button>
+      {/if}
+      <!-- <Button on:click={login()}>Login</Button> -->
+    </nav>
+  </div>
   <!-- <nav>
     <a href="#">Home</a>
   </nav> -->
@@ -46,12 +52,25 @@
     margin-top: 1.5rem;
     margin-bottom: 2rem;
   }
-  
+
   @media (min-width: 66rem) {
     header {
       max-width: 66rem;
       margin-left: auto;
       margin-right: auto;
     }
+  }
+
+  .nav__container {
+    /* background-color: color(var(--theme-background) blackness(+ 80%)); */
+    background-color: color(var(--theme-background) tint(- 80%));
+    border-radius: 0.5rem;
+  }
+
+  nav {
+    display: grid;
+    grid-auto-flow: column;
+    grid-gap: 1.5rem;
+    font-family: Cabin;
   }
 </style>
