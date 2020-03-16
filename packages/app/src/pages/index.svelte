0dced6cbc79685ed170aa1c2c3e5ae66cda4780f
josefaidt <josef.aidt@gmail.com>
2020-03-16 18:11:57 -0500

    feat(app): add routing

---
+++ packages/app/src/pages/index.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Form from '../components/Form/Form.svelte'
+</script>
+
+<slot>
+  <Form />
+  <div class="parks">
+    <!-- {#each parkData.data as park}
+      <ParkCard parkData={park} />
+    {/each} -->
+  </div>
+</slot>
