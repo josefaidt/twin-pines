53209f0a93630700cfb06ce6cdf1ad701b906633
josefaidt <josef.aidt@gmail.com>
2020-03-28 09:29:53 -0500

    feat: add generic user info to profile page and auth ctx

--- packages/app/src/pages/UserProfile.svelte
+++ packages/app/src/pages/UserProfile.svelte
@@ -1,12 +1,20 @@
 <script>
   import { getContext } from 'svelte'
 
-  let user = getContext('auth')
+  let { userInfo: user } = getContext('auth')
+  console.log('USER IS', user)
 </script>
 
 <slot>
   <section>
     <!-- <h1>{$user.name}</h1> -->
-    <h1>User Profile</h1>
+    <header>
+      <h1>User Profile</h1>
+    </header>
+    <div>
+      <p>Hello, {$user.nickname}</p>
+      <p>Verified? {$user.email_verified}</p>
+      <img src={$user.picture} alt="profile picture">
+    </div>
   </section>
 </slot>
